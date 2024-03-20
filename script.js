b72d6b5c2fac3497981796d90874c89f8590ecfc
JuanCAnderson <andersonjuancruz97@gmail.com>
2024-03-20 00:44:19 -0300

    Proyecto Cabañas

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+document.querySelector(".footer h2").addEventListener("click", () =>{
+    scroll.scrollTo(0)
+})
+
+
+var elems = document.querySelectorAll(".elem")
+var page2 = document.querySelector("#page2")
+elems.forEach(function(ele){
+    ele.addEventListener("mouseenter", function(){
+        var bgimg = ele.getAttribute("data-img")
+        page2.style.backgroundImage = `url(${bgimg})`
+    })
+})
